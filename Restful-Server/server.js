f2a0933416d11808ee337c167984e298759eed2f
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-05-15 07:07:16 -0400

    creating server

---
+++ Restful-Server/server.js
@@ -0,0 +1,20 @@
+const express = ('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/api/data', (req, res) => {
+    const data = {
+        message: 'This is some data from the server',
+    };
+    res.json(data);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running http://localhost:${port}`);
+});
+
